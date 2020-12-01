def6fd3a3f27e167b91c21ce5509896def0f443b
Amitendra Singh <63096147+amitendra9@users.noreply.github.com>
2020-12-01 09:09:02 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,37 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Bank Of TSF</title>
-	<meta charset="utf-8">
-    	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<?php require_once ("bootstrap.php")?>
-</head>
-<body class="bg-primary text-white">
-    <header class="bg-dark">
-        <img src="img/Logo.png"  alt="Logo" height="72dp" class="float-left">
-        <h1 class="display-3 font-weight-bold">BANK OF TSF</h1>    
-    </header>
-    <nav class="pl-5 bg-dark">
-            <ul class="nav">
-            <li class="ml-5 mr-3 mb-3 col-2 nav-item text-center border border-raduis border-white rounded-lg"><a href="index.php" class="nav-link">Home</a></li>
-            
-            <li class="ml-5 mr-3 mb-3 col-2 nav-item text-center border border-raduis border-white rounded-lg"><a href="all.php" class="nav-link">View All Customer</a></li>
-            <li class="ml-5 mr-5 mb-3 col-2 nav-item text-center border border-raduis border-white rounded-lg"><a href="trans.php" class="nav-link">Transfer Money</a></li>
-            <li class="ml-5 mr-5 mb-3 col-2 nav-item text-center border border-raduis border-white rounded-lg"><a href="history.php" class="nav-link">Transfer History</a></li>
-            </ul> 
-    </nav>
+<?php require_once ("header.php")?>
    <!-- -->
    <div class="container col-8 p-3 mt-3">
 		<a href="all.php" class="p-3 text-decoration-none bg-dark text-white border border-raduis border-white rounded-lg text-center">View All Customers</a>
 	</div>
-    <footer class="bg-dark col fixed-bottom">
-        <h4 class="font-weight-bold">By Amitendra Singh</h4>
-        <p>
-            <a href="https://github.com/amitendra9"><img src="https://img.icons8.com/ios-filled/2x/github-2.png" alt="" height="36dp"/></a>    
-            <a href="https://auth.geeksforgeeks.org/user/amitendra9/profile"><img src="https://img.icons8.com/color/2x/GeeksforGeeks.png" alt="" height="36dp"/></a>     
-            <a href="https://www.youtube.com/channel/UC7UgGDO79n7G8iHVVikmVZA?view_as=subscriber"><img src="https://img.icons8.com/metro/2x/youtube.png" alt="" height="36dp"/></a>      
-            <a href="https://www.facebook.com/amitendra.singh.142/"><img src="img/fb.png" alt=""/></a>
-        </p>
-    </footer>
-</body>
-</html>
+<?php require_once ("footer.php")?>
