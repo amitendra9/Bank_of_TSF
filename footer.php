d44667d7ddbc60fbf12d79385eee77e401c02854
Amitendra Singh <63096147+amitendra9@users.noreply.github.com>
2020-12-01 09:04:10 +0530

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,12 @@
+<footer class="bg-dark  p-3">
+    
+        <h4 class="font-weight-bold">By Amitendra Singh</h4>
+        <p>
+            <a href="https://github.com/amitendra9"><img src="https://img.icons8.com/ios-filled/2x/github-2.png" alt="" height="36dp"/></a>    
+            <a href="https://auth.geeksforgeeks.org/user/amitendra9/profile"><img src="https://img.icons8.com/color/2x/GeeksforGeeks.png" alt="" height="36dp"/></a>     
+            <a href="https://www.youtube.com/channel/UC7UgGDO79n7G8iHVVikmVZA?view_as=subscriber"><img src="https://img.icons8.com/metro/2x/youtube.png" alt="" height="36dp"/></a>      
+            <a href="https://www.facebook.com/amitendra.singh.142/"><img src="img/fb.png" alt=""/></a>
+        </p>
+    </footer>
+</body>
+</html>
