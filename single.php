2d098fa42bdc14d96d71d70fcf6d5526d49186a0
Amitendra Singh <63096147+amitendra9@users.noreply.github.com>
2020-12-01 09:07:52 +0530

    Update single.php

--- single.php
+++ single.php
@@ -1,36 +1,4 @@
-<?php
-require_once "pdo.php";
-session_start();
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    
-    <title>Bank Of TSF</title>
-	<meta charset="utf-8">
-    	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<?php require_once ("bootstrap.php")?>
-</head>
-
-<body class="bg-primary text-white">
-	<!-- -->
-    <header class="bg-dark">
-        <img src="img/Logo.png"  alt="Logo" height="72dp" class="float-left">
-        
-        <h1 class="display-3 font-weight-bold">BANK OF TSF</h1>
-        
-    </header>
-
-	<!-- -->
-    <nav class="pl-5 bg-dark">
-            <ul class="nav">
-            <li class="ml-5 mr-3 mb-3 col-2 nav-item text-center border border-raduis border-white rounded-lg"><a href="index.php" class="nav-link">Home</a></li>
-            
-            <li class="ml-5 mr-3 mb-3 col-2 nav-item text-center border border-raduis border-white rounded-lg"><a href="all.php" class="nav-link">View All Customer</a></li>
-            <li class="ml-5 mr-5 mb-3 col-2 nav-item text-center border border-raduis border-white rounded-lg"><a href="trans.php" class="nav-link">Transfer Money</a></li>
-            <li class="ml-5 mr-5 mb-3 col-2 nav-item text-center border border-raduis border-white rounded-lg"><a href="history.php" class="nav-link">Transfer History</a></li>
-            </ul>     
-    </nav>
+<?php require_once ("header.php")?>
 	<div class="container p-3 mt-5 mb-5 bg-dark col-6 border border-white">
 		<?php 
 			$stmt = $pdo->query("SELECT * FROM customers");
@@ -92,15 +60,4 @@ session_start();
 		<a href="trans.php">Transfer Money</a>
 		</div>
 	</div>
-    <footer class="bg-dark p-3">
-    
-        <h4 class="">By Amitendra Singh</h4>
-        <p>
-            <a href="https://github.com/amitendra9"><img src="https://img.icons8.com/ios-filled/2x/github-2.png" alt="" height="36dp"/></a>    
-            <a href="https://auth.geeksforgeeks.org/user/amitendra9/profile"><img src="https://img.icons8.com/color/2x/GeeksforGeeks.png" alt="" height="36dp"/></a>     
-            <a href="https://www.youtube.com/channel/UC7UgGDO79n7G8iHVVikmVZA?view_as=subscriber"><img src="https://img.icons8.com/metro/2x/youtube.png" alt="" height="36dp"/></a>      
-            <a href="https://www.facebook.com/amitendra.singh.142/"><img src="img/fb.png" alt=""/></a>
-        </p>
-    </footer>
-</body>
-</html>
+<?php require_once ("footer.php")?>
