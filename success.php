996c2459062f99c4b22fa82090bd46328d610583
Amitendra Singh <63096147+amitendra9@users.noreply.github.com>
2020-12-01 09:05:28 +0530

    Update success.php

--- success.php
+++ success.php
@@ -61,15 +61,4 @@
             <input type="submit" value="Back to Home" class="p-3 bg-primary text-white border border-raduis border-white rounded-lg text-center">
         </form>
 	</div>
-    <footer class="bg-dark  p-3">
-    
-        <h4 class="font-weight-bold">By Amitendra Singh</h4>
-        <p>
-            <a href="https://github.com/amitendra9"><img src="https://img.icons8.com/ios-filled/2x/github-2.png" alt="" height="36dp"/></a>    
-            <a href="https://auth.geeksforgeeks.org/user/amitendra9/profile"><img src="https://img.icons8.com/color/2x/GeeksforGeeks.png" alt="" height="36dp"/></a>     
-            <a href="https://www.youtube.com/channel/UC7UgGDO79n7G8iHVVikmVZA?view_as=subscriber"><img src="https://img.icons8.com/metro/2x/youtube.png" alt="" height="36dp"/></a>      
-            <a href="https://www.facebook.com/amitendra.singh.142/"><img src="img/fb.png" alt=""/></a>
-        </p>
-    </footer>
-</body>
-</html>
+    <?php require_once ("footer.php")?>
